8febdd1c77526e0772d0d52fbb8cf09f875cb7eb
Pierre-Cognard <pierre.cognard@alumni.univ-avignon.fr>
2023-05-14 16:11:47 +0200

    choix position + service, design

--- app/src/main/java/com/example/smartswitch/MainActivity.kt
+++ app/src/main/java/com/example/smartswitch/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.smartswitch
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
@@ -20,6 +21,8 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    var currentPosition = 0
+    var currentService = 0
     private lateinit var currentTimeTextView: TextView
     private val handler = Handler(Looper.getMainLooper())
     private val updateTimeRunnable = object : Runnable {
@@ -34,6 +37,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val useWifiButton = findViewById<Button>(R.id.useWifiButton)
+        val useCellularButton = findViewById<Button>(R.id.useCellularButton)
+        val GPSButton = findViewById<Button>(R.id.GPS_Button)
+        val serviceButton = findViewById<Button>(R.id.Service_Button)
+
         val batteryLevel = getBatteryLevel(this)
         val batteryLevelTextView = findViewById<TextView>(R.id.batterie)
         batteryLevelTextView.text = "$batteryLevel%"
@@ -41,19 +49,57 @@ class MainActivity : AppCompatActivity() {
         currentTimeTextView = findViewById(R.id.heure)
         handler.post(updateTimeRunnable)
 
-        val useWifiButton = findViewById<Button>(R.id.useWifiButton)
         useWifiButton.setOnClickListener {
             getSpecificNetwork(this, NetworkCapabilities.TRANSPORT_WIFI) { network ->
                 forceUseSpecificNetwork(this, network)
             }
         }
 
-        val useCellularButton = findViewById<Button>(R.id.useCellularButton)
         useCellularButton.setOnClickListener {
             getSpecificNetwork(this, NetworkCapabilities.TRANSPORT_CELLULAR) { network ->
                 forceUseSpecificNetwork(this, network)
             }
         }
+
+        GPSButton.setOnClickListener {
+            val mBuilder = AlertDialog.Builder(this)
+            mBuilder.setTitle("Position GPS")
+            mBuilder.setSingleChoiceItems(R.array.positionsGPS, currentPosition) { dialog, language ->
+                when(language){
+                    0 -> setPosition(0)
+                    1 -> setPosition(1)
+                    2 -> setPosition(2)
+                    3 -> setPosition(3)
+                    4 -> setPosition(4)
+                    5 -> setPosition(5)
+                    6 -> setPosition(6)
+                    7 -> setPosition(7)
+                    8 -> setPosition(8)
+                    9 -> setPosition(9)
+                }
+                dialog.dismiss()
+            }
+            val mDialog = mBuilder.create()
+            mDialog.show()
+        }
+
+        serviceButton.setOnClickListener {
+            val mBuilder = AlertDialog.Builder(this)
+            mBuilder.setTitle("Service")
+            mBuilder.setSingleChoiceItems(R.array.services, currentService) { dialog, language ->
+                when(language){
+                    0 -> setService(0)
+                    1 -> setService(1)
+                    2 -> setService(2)
+                }
+                dialog.dismiss()
+            }
+            val mDialog = mBuilder.create()
+            mDialog.show()
+        }
+
+        setPosition(0)
+        setService(0)
 
     }
 
@@ -107,7 +153,21 @@ class MainActivity : AppCompatActivity() {
 
     fun forceUseSpecificNetwork(context: Context, network: Network?) {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        connectivityManager.bindProcessToNetwork(network)
+        //connectivityManager.bindProcessToNetwork(network)
+    }
+
+    private fun setPosition(pos: Int){
+        val position = findViewById<TextView>(R.id.positionTextView)
+        currentPosition = pos
+        System.out.println(resources.getStringArray(R.array.positionsGPS)[pos])
+        position.text = resources.getStringArray(R.array.positionsGPS)[pos]
+    }
+
+    private fun setService(serv: Int){
+        val service = findViewById<TextView>(R.id.serviceTextView)
+        currentService = serv
+        System.out.println(resources.getStringArray(R.array.services)[serv])
+        service.text = resources.getStringArray(R.array.services)[serv]
     }
 }
 
